eaeb536a2356b9fed03bccc1eaaff125562d0a63
Yusufkr03 <yusufkr2903@gmail.com>
2024-10-15 23:04:39 +0300

    Add files via upload

---
+++ Soru1-İki sayı farkı/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number1 = 563;
+int number2 = 123;
+int result = number1 - number2;
+Console.WriteLine(result);
+Console.ReadKey();
